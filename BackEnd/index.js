2cec67735580b13be9c789485fa1f9e0f14356e9
McBaque <mcbaque01@gmail.com>
2024-02-19 15:36:52 +0800

    my First Commit

---
+++ BackEnd/index.js
@@ -0,0 +1,27 @@
+import express from "express"; // nodejs framework
+
+const app = express();
+const port = 8080;
+app.use(express.json());
+
+app.post('/signup', async (req, res) => {
+    console.log("VERCEL API");
+    console.log("Hi there");
+    const { firstname, lastname, name, email, password } = req.body;
+    const newuser = { firstname, lastname, name, email, password };
+    try {
+        // const result = await Users.insertOne(newuser);
+        // console.log('Inserted document:', result.insertedId);
+        res.status(200).json({ response: { message: 'User created successfully', input: {...newuser} } });
+    }
+    catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Internal Server Error' });
+    }
+});
+
+app.listen(port, () => {
+console.log(`Server is running on ${port}`);
+});
+
+export default app;
